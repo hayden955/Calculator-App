8be241bc32ea921c6a0996268e04b617729ae882
Hayden Jennings <haydensj@bu.edu>
2023-11-24 12:23:16 -0800

    Create color styles and initial components page

--- Calculator/App.js
+++ Calculator/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Calculator App!</Text>
+      <Text style={styles.heading}>Calculator</Text>
+      <Text style={styles.description}>A React Native Project by Hayden Jennings</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +13,19 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    display: 'flex',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    height: '100%'
+  },
+  heading: {
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  description: {
+    textTransform: 'uppercase',
+    fontSize: 12,
   },
 });
---
+++ Calculator/src/context/ThemeContext.tsx
@@ -0,0 +1,4 @@
+import { createContext } from 'react';
+
+//we can switch between light and dark theme
+export const ThemeContext = createContext('light');
