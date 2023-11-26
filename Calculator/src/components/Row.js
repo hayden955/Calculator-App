268440937100af324800029dea8ccc091f263c60
Hayden Jennings <haydensj@bu.edu>
2023-11-25 22:01:02 -0800

    built row file

---
+++ Calculator/src/components/Row.js
@@ -0,0 +1,11 @@
+import { StyleSheet, View } from "react-native"; 
+
+const Row = ({children}) => {
+  return <View style={styles.container}>{View}</View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+  },
+});
