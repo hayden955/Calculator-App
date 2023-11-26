200257a953a3939197dbc4f3dc0d95406d9bf3c2
Hayden Jennings <haydensj@bu.edu>
2023-11-25 22:31:46 -0800

    Start on calculator logic

--- Calculator/src/util/calculator.js
+++ Calculator/src/util/calculator.js
@@ -1,1 +1,48 @@
 //calculator logic
+
+
+export const clearCase = {
+  currValue: "0",
+  operator: null,
+  prevValue: null,
+};
+
+export const numberCase = (value, state) => {
+  if (state.currValue === "0"){
+    return {currValue: '${value}'};
+  }
+  return {currValue: '${state.currValue}${value}'};
+};
+
+const answerCase = (state) => {
+  const {currValue, prevValue, operator} = state;
+
+  const curr = parseFloat(currValue);
+  const prev = parseFloat(prevValue);
+  const resetState = {operator: null, prevValue: null};
+
+  //operatation cases
+  switch(operator){
+    case "+":
+      return {
+        currValue: '${prev + curr}',
+        ...resetState,
+      };
+      case "-":
+        return {
+          currValue: '${prev - curr}',
+          ...resetState,
+        };
+    case "*":
+      return {
+        currValue: '${prev * curr}',
+        ...resetState,
+      };
+    case "/":
+      return {
+        currValue: '${prev / curr}',
+        ...resetState,
+      };
+  }
+  
+}
